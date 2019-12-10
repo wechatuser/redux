f313601536db992e0f30fd7e1dd819bcb0788d66
wechatuser <56934041+wechatuser@users.noreply.github.com>
2019-12-10 14:36:39 -0500

    Add files via upload

---
+++ src/store/actionCreators.js
@@ -0,0 +1,30 @@
+import { CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM, INIT_LIST } from './actionTypes';
+
+export const getInputChangeAction = (value) => ({
+	type: CHANGE_INPUT_VALUE,
+	value
+});
+
+export const getAddItemAction = () => ({
+	type: ADD_TODO_ITEM
+});
+
+export const getDeleteItemAction = (index) => ({
+	type: DELETE_TODO_ITEM,
+	index
+});
+
+export const initListAction = (data) => ({
+	type: INIT_LIST,
+	data
+});
+
+export const getTodoList = () => {
+	return () => {
+		axios.get('/list.json').then((res)=>{
+			const data = res.data;
+			const action = initListAction(data);
+			dispatch(action);
+		})
+	}
+}
